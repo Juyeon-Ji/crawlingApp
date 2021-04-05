e6d79cf02448461d01b731ad56d79202c5e4eb31
PBBOY <arelyoul@naver.com>
2021-04-05 13:41:16 +0900

    selenium driver singleton 으로 변경

--- proj/app/appmain.py
+++ proj/app/appmain.py
@@ -22,7 +22,7 @@ from proj.app.crawl.categorycrawl import CategoryCrawl
 from proj.app.crawl.productcrawl import ProductCrawl
 from proj.common.database.dbmanager import DatabaseManager
 from proj.common.config.configmanager import ConfigManager
-
+from proj.common.driver.seleniumdriver import Selenium
 
 def close(driver):
     if driver is not None:
@@ -65,28 +65,28 @@ def main():
     configmanager = ConfigManager()
     databasemanager = DatabaseManager(configmanager.database_object_list)
 
-    CHROMEDRIVER_PATH = '../resource/chromedriver.exe'
-    # CHROMEDRIVER_PATH = 'D:/_1.project/WEBuilder/python_project/git_crawling/crawlingApp/proj/resource/chromedriver.exe'
-
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-gpu")
-    # chrome_options.add_argument( f"--window-size={ WINDOW_SIZE }" )
-    
-    driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
-
+    # CHROMEDRIVER_PATH = '../resource/chromedriver.exe'
+    # # CHROMEDRIVER_PATH = 'D:/_1.project/WEBuilder/python_project/git_crawling/crawlingApp/proj/resource/chromedriver.exe'
+    #
+    # chrome_options = Options()
+    # chrome_options.add_argument("--headless")
+    # chrome_options.add_argument("--no-sandbox")
+    # chrome_options.add_argument("--disable-gpu")
+    # # chrome_options.add_argument( f"--window-size={ WINDOW_SIZE }" )
+    #
+    # driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+    driver = Selenium().driver
     categorycrawl = CategoryCrawl(driver=driver,
                                   crawl_config=configmanager.crawl_config_object)
 
     categorycrawl.parse(databasemanager.insert_one_mongo)
 
-    json_datas = databasemanager.find_all_mongo('category')
-
-    category = ProductCrawl(driver=driver,
-                            json_data=json_datas,
-                            func=databasemanager.insert_many_mongo,
-                            crawl_config=configmanager.crawl_config_object)
+    # json_datas = databasemanager.find_all_mongo('category')
+    #
+    # category = ProductCrawl(driver=driver,
+    #                         json_data=json_datas,
+    #                         func=databasemanager.insert_many_mongo,
+    #                         crawl_config=configmanager.crawl_config_object)
 
     logger.info('Crawl Test End')
     close(driver)
--- proj/common/driver/seleniumdriver.py
+++ proj/common/driver/seleniumdriver.py
@@ -1,11 +1,33 @@
+import logging
 from selenium import webdriver
+
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.webdriver import WebDriver
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-gpu")
-# chrome_options.add_argument( f"--window-size={ WINDOW_SIZE }" )
 
-CHROMEDRIVER_PATH = '../resource/chromedriver.exe'
-driver = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+class Singleton(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        else:
+            pass
+            # 매번 __init__ 호출하고 싶으면 아래 주석 제거
+            # cls._instances[cls].__init__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Selenium(object, metaclass=Singleton):
+    driver: WebDriver = None
+
+    def __init__(self):
+        logging.info('Selenium Driver init')
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-gpu")
+
+        _CHROMEDRIVER_PATH = '../resource/chromedriver.exe'
+        self.driver = webdriver.Chrome(executable_path=_CHROMEDRIVER_PATH, chrome_options=chrome_options)
+
